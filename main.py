4442605ec9b4aa629bc1c64063b29d87fbb5d534
YaN9-Y <40831733+YaN9-Y@users.noreply.github.com>
2019-11-25 20:52:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import torch
 import argparse
 from shutil import copyfile
 from src.config import Config
-from src.edge_connect import EdgeConnect
+from src.lafin import Lafin
 
 
 def main(mode=None):
@@ -45,7 +45,7 @@ def main(mode=None):
 
 
     # build the model and initialize
-    model = EdgeConnect(config)
+    model = Lafin(config)
     model.load()
 
 
@@ -75,14 +75,7 @@ def load_config(mode=None):
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', '--checkpoints', type=str, default='./checkpoints', help='model checkpoints path (default: ./checkpoints)')
-    parser.add_argument('--model', type=int, choices=[1, 2, 3, 4], help='1: edge model, 2: inpaint model, 3: edge-inpaint model, 4: joint model')
-
-    # test mode
-    if mode == 2:
-        parser.add_argument('--input', type=str, help='path to the input images directory or an input image')
-        parser.add_argument('--mask', type=str, help='path to the masks directory or a mask file')
-        parser.add_argument('--edge', type=str, help='path to the edges directory or an edge file')
-        parser.add_argument('--output', type=str, help='path to the output directory')
+    parser.add_argument('--model', type=int, choices=[1, 2, 3], help='1: landmark prediction model, 2: inpaint model, 3: joint model')
 
     args = parser.parse_args()
     config_path = os.path.join(args.path, 'config.yml')
